6d77796789277e108ef4ddb41e3b76fe9a034a84
Kevin Jiang <kevinjiang1230@gmail.com>
2017-09-28 17:48:50 -0400

    third commid

--- src/com/gf/Main.java
+++ src/com/gf/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Modified");
+        System.out.println("Third Modified");
 
     }
 }
