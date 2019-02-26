04c50d930b666063f15cbac1c6da70e56d22c0d1
Administrator <jiangfeng1230@msn.com>
2019-02-26 15:34:18 +0800

    fff
    Signed-off-by: Administrator <jiangfeng1230@msn.com>

--- src/com/gf/Main.java
+++ src/com/gf/Main.java
@@ -6,6 +6,6 @@ public class Main {
         System.out.println("Modified");
         System.out.println("Third Modified");
         System.out.println("Branch modified");
-        System.out.println("branch1")
+        System.out.println("branch1");f
     }
 }
